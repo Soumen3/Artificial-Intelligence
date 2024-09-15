9da48ebc6a5709356727b38ca01c39d53a6aa93d
Soumen3 <soumensamanta112233@gmail.com>
2024-09-15 18:14:04 +0530

    ready for deploy the bengaluru home price prediction model

--- Projects/BHK/client/app.js
+++ Projects/BHK/client/app.js
@@ -1,7 +1,8 @@
 
 function onPageLoad() {
 	console.log("Document loaded");
-	var url = "http://127.0.0.1:5000/get_location_names";
+	// var url = "http://127.0.0.1:5000/get_location_names";
+	var url = '/api/get_location_names';
 	$.get(url, function(data, status) {
 		console.log("Got response for get_location_names request");
 		if(data) {
@@ -48,7 +49,8 @@ function onClickedEstimatePrice(){
 	var location = document.getElementById("uiLocations");
 	var estPrice = document.getElementById("uiEstimatedPrice");
 
-	var url = "http://127.0.0.1:5000//predict_home_price";
+	// var url = "http://127.0.0.1:5000//predict_home_price";
+	var url = '/api/predict_home_price';
 
 	$.post(url, {
 			total_sqft: parseFloat(sqft.value),
